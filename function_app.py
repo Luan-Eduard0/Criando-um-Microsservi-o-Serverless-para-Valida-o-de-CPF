ab0b2df340f26fb983cb2ac6d2a6ba7077fbcb43
Luan Eduardo <96151861+Luan-Eduard0@users.noreply.github.com>
2025-02-01 21:44:26 -0300

    Az-204 desafio

--- function_app.py
+++ function_app.py
@@ -1,48 +1,33 @@
-import azure.functions as func
+import azure.functions as az
 import logging
+import re
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+def isValidCpf(cpf: str) -> bool:
+    cpf = re.sub(r'\D', '', cpf)  
+    
+    if len(cpf) != 11 or cpf == cpf[0] * 11:
+        return False
+    
+    def calculateDigit(cpf_slice, factor):
+        total = sum(int(digit) * (factor - i) for i, digit in enumerate(cpf_slice))
+        return (total * 10 % 11) % 10
+    
+    return (
+        calculateDigit(cpf[:9], 10) == int(cpf[9]) and
+        calculateDigit(cpf[:10], 11) == int(cpf[10])
+    )
+
+app = az.FunctionApp(http_auth_level=az.AuthLevel.FUNCTION)
 
 @app.route(route="cpfvalidator")
-def cpfvalidator(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def cpfvalidator(req: az.HttpRequest) -> az.HttpResponse:
 
-    cpf = req.params.get('cpf')
-    if not cpf:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            cpf = req_body.get('cpf')
-
-    if cpf and is_valid_cpf(cpf):
-        return func.HttpResponse(f"The CPF {cpf} is valid.")
-    else:
-        return func.HttpResponse(
-             "Invalid CPF. Please provide a valid CPF in the query string or in the request body.",
-             status_code=400
-        )
-
-def is_valid_cpf(cpf: str) -> bool:
-    # Remove any non-numeric characters
-    cpf = ''.join(filter(str.isdigit, cpf))
-
-    # CPF must be 11 digits long
-    if len(cpf) != 11:
-        return False
-
-    # Check for CPF with all digits the same (e.g., 111.111.111-11)
-    if cpf == cpf[0] * len(cpf):
-        return False
-
-    # Calculate the first check digit
-    sum1 = sum(int(cpf[i]) * (10 - i) for i in range(9))
-    check_digit1 = (sum1 * 10 % 11) % 10
-
-    # Calculate the second check digit
-    sum2 = sum(int(cpf[i]) * (11 - i) for i in range(10))
-    check_digit2 = (sum2 * 10 % 11) % 10
-
-    # Check if the calculated check digits match the input
-    return check_digit1 == int(cpf[9]) and check_digit2 == int(cpf[10])
+    try:
+        cpf = req.params.get('cpf') or (req.get_json().get('cpf') if req.get_json(silent=True) else None)
+        
+        if cpf and isValidCpf(cpf):
+            return az.HttpResponse(f"CPF: {cpf} é valido")
+        return az.HttpResponse("CPF INVALIDO.", status_code=400)
+    except Exception as e:
+  
+        return az.HttpResponse("Internal Server Error", status_code=500)
